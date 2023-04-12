9dec955201115b1c930a54ac266fcb7bf05082b0
Magda Pich <72314614+magdap1601@users.noreply.github.com>
2023-04-13 01:13:10 +0200

    feat: create and deploy website

---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "google_storage_bucket" "static-site" {
+  name          = "static-website-terraform"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "./website/index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
+
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.static-website.name
+  role   = "READER"
+  entity = "allUsers"
+}
+resource "google_storage_bucket_object" "picture" {
+  name   = "img1.jpg"
+  source = "./website/img1.jpg"
+  bucket = "static-website-terraform"
+}
+resource "google_storage_bucket_object" "style" {
+  name   = "styles.css"
+  source = "./website/styles.css"
+  bucket = "static-website-terraform"
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project = "evident-zone-377721"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
