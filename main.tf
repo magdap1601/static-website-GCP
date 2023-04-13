7f1976386985b1623b5db1d64a246741ef6de1e0
Magda Pich <72314614+magdap1601@users.noreply.github.com>
2023-04-13 10:01:10 +0200

    fix: delete unform bucket elevel acces

--- main.tf
+++ main.tf
@@ -3,8 +3,6 @@ resource "google_storage_bucket" "static-site" {
   location      = "EU"
   force_destroy = true
 
-  uniform_bucket_level_access = true
-
   website {
     main_page_suffix = "./website/index.html"
     not_found_page   = "404.html"
